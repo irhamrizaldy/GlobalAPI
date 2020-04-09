1de24b5c5f2c98e065d5828d8abb9e241daf00af
irhamrizaldy <rirham12@gmail.com>
2020-04-09 19:30:22 +0700

    change function to call listArtikel.json

--- src/index.js
+++ src/index.js
@@ -3,12 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import HelloComponent from './component/HelloComponent';
+import StateFullComponent from './container/StateFullComponent';
+import BlogPost from './container/BlogPost/BlogPost';
+// function HelloWorld(){
+//   return <p> ini adalah function component </p>
+// }
+
+// const HelloWorld = () => {
+//   return <p> ini adalah arrow function </p>
+// }
+
+// class StateFullcomponent extends React.Component {
+//   render() {
+//     return <p> ini adalah Statefull Component </p>
+//   }
+// }
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BlogPost />,
+  document.getElementById('content')
 );
 
 // If you want your app to work offline and load faster, you can change
